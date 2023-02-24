8a849ade0f03ee26eb39e459dd45e7c78eb3ef7e
martex-dev <adedayoniyio@gmail.com>
2023-02-24 21:54:03 +0100

    changed user to admin

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyAppState extends State<MyApp> {
       theme: theme.getTheme(),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Provider.of<UserProvider>(context).user.token.isNotEmpty
-          ? Provider.of<UserProvider>(context).user.type == 'user'
+          ? Provider.of<UserProvider>(context).user.type == 'admin'
               ? const BottomBar()
               : const AdminScreen()
           : const LandingScreen(),
